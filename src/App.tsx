12bbc6df5e1dd8d9044c55df0b64bd9e390b69b4
Guner <Guneruh@gmail.com>
2023-03-01 19:57:30 +0000

    the mexican app

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const mexican: String= "Im mexican, hello putos"
+
+
+
+
+
+  return (
+    <div className="App">
+     {mexican}
+    </div>
+  );
+}
+
+export default App;
