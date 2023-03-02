20a037a478af0c9e91936512b559e8a826a73039
Vitor Quintaos <vitorquintaos@gmail.com>
2023-03-02 19:00:22 +0000

    FIxed project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import React from 'react'
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const mexican: String= "Im mexican, hello putos"
-
-
-
-
-
   return (
     <div className="App">
-     {mexican}
+      {mexican}
     </div>
   );
 }
